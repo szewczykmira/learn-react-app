048f60aa85f0643c602dc5606aad78a546b3fb12
Mira Szewczyk <szewczyk.mira@gmail.com>
2018-07-24 13:51:36 +0200

    Display current time

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentDate: null };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        currentDate: new Date().toLocaleString()
+      });
+    }, 1000);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          Hello world!
-        </p>
+        <p className="App-intro">{this.state.currentDate}</p>
       </div>
     );
   }
