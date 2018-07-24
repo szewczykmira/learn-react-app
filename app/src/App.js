3d35becfb0784cc166340f2256ac1ecf79303c52
Mira Szewczyk <szewczyk.mira@gmail.com>
2018-07-24 14:09:11 +0200

    Display time in main component

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Time from "./Time.js";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { currentDate: null };
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        currentDate: new Date().toLocaleString()
-      });
-    }, 1000);
-  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.currentDate}</p>
+        <p className="App-intro"><Time /></p>
       </div>
     );
   }
