31b1952e94cc670001725077962c7bd8fe69d328
Mira Szewczyk <szewczyk.mira@gmail.com>
2018-07-25 14:35:35 +0200

    Add navbar component

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import CountriesList from "./Countries.js";
-import Time from "./Time.js";
+import Navbar from "./Navbar.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Time />
+        <Navbar />
         <CountriesList />
       </div>
     );
---
+++ app/src/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Time from "./Time";
+
+class Navbar extends Component {
+  render(){
+    return <Time />;
+  }
+}
+
+export default Navbar;
