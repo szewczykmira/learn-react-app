2594b2f255a9a0723b3cdad4120badd2feb63855
Mira Szewczyk <szewczyk.mira@gmail.com>
2018-07-25 14:40:14 +0200

    Style navbar

--- app/src/Navbar.js
+++ app/src/Navbar.js
@@ -2,8 +2,14 @@ import React, { Component } from "react";
 import Time from "./Time";
 
 class Navbar extends Component {
-  render(){
-    return <Time />;
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <span className="navbar-brand" href="#">
+          <Time />
+        </span>
+      </nav>
+    );
   }
 }
 
